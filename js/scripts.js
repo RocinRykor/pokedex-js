9ead5f1f39aee70d06310dcdcaf50dc582a69602
Steven <Briggs.steven369@gmail.com>
2022-12-01 02:12:45 -0800

    Exercise 2 - Created pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert("Hello World!");
-
-let favoriteFood = "Pizza";
-document.write(favFood);
+let pokemonList = [
+    {
+        name: "Growlithe",
+        pokedexNumber: 58,
+        height: 28,
+        types: ["fire"],
+    },
+    {
+        name: "Servine",
+        pokedexNumber: 496,
+        height: 31,
+        types: ["grass"],
+    },
+    {
+        name: "Chandelure",
+        pokedexNumber: 609,
+        height: 39,
+        types: ["ghost", "fire"],
+    },
+];
