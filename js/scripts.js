1f7ddade6ab34353bc2cc3079cb4fe62d54b3bf6
Steven <Briggs.steven369@gmail.com>
2022-12-01 02:46:29 -0800

    Exercise 3 - For-Loop to write pokemon to page

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,22 @@ let pokemonList = [
         types: ["ghost", "fire"],
     },
 ];
+
+//Initalize varibles to be used later
+let heightThreshold = 35; //Anything this size or over is a big pokemon
+
+// Create a for-loop to go over each pokemon in the list
+for (let index = 0; index < pokemonList.length; index++) {
+    const pokemon = pokemonList[index];
+
+    let isBig = pokemon.height >= heightThreshold; //Checks wether or not the pokemon is over the height threshold
+
+    let outputString = `${pokemon.name} (${pokemon.height} in.)`; //Setting the base string to be output
+
+    if (isBig) {
+        //if the pokemon is big
+        outputString += " - Wow! is big!"; //add a message to the end of the original string
+    }
+
+    document.writeln(outputString + "<br>"); //Writes the message to the page and adds a new line break.
+}
