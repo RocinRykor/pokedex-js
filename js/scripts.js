71eb873add259179cdaac154ace918cfe2c0cbda
Steven <Briggs.steven369@gmail.com>
2022-12-02 11:40:27 -0800

    Exercise 5 -
    Wrapped pokemonList in IIFE
    changed for loop to ForEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,51 @@
-let pokemonList = [
-    {
-        name: "Growlithe",
-        pokedexNumber: 58,
-        height: 28,
-        types: ["fire"],
-    },
-    {
-        name: "Servine",
-        pokedexNumber: 496,
-        height: 31,
-        types: ["grass"],
-    },
-    {
-        name: "Chandelure",
-        pokedexNumber: 609,
-        height: 39,
-        types: ["ghost", "fire"],
-    },
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Growlithe",
+            pokedexNumber: 58,
+            height: 28,
+            types: ["fire"],
+        },
+        {
+            name: "Servine",
+            pokedexNumber: 496,
+            height: 31,
+            types: ["grass"],
+        },
+        {
+            name: "Chandelure",
+            pokedexNumber: 609,
+            height: 39,
+            types: ["ghost", "fire"],
+        },
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+    };
+})();
 
 //Initalize varibles to be used later
 let heightThreshold = 35; //Anything this size or over is a big pokemon
 
-// Create a for-loop to go over each pokemon in the list
-for (let index = 0; index < pokemonList.length; index++) {
-    const pokemon = pokemonList[index];
-
+pokemonRepository.getAll().forEach(function printDetails(pokemon) {
     let isBig = pokemon.height >= heightThreshold; //Checks wether or not the pokemon is over the height threshold
 
-    let outputString = `${pokemon.name} (${pokemon.height} in.)`; //Setting the base string to be output
+    document.write(`${pokemon.name} (${pokemon.height} in.)`); //Writing the base string for the pokemon
 
     if (isBig) {
         //if the pokemon is big
-        outputString += " - Wow! is big!"; //add a message to the end of the original string
+        document.write(" - Wow! They are pretty big!"); //Appending the special identifier
     }
 
-    document.writeln(outputString + "<br>"); //Writes the message to the page and adds a new line break.
-}
+    document.write("<br>"); //Finalizing  with a line break
+});
